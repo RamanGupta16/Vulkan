c106a787bc9856f9876fd8c3a79a3a0a77da1b82
RamanGupta16 <ramangupta16@gmail.com>
2025-09-27 13:55:51 +0530

    Index Buffer

--- main.cpp
+++ main.cpp
@@ -178,11 +178,25 @@ vec3 colors[3] = vec3[](
     vec3(1.0, 0.0, 0.0),
     vec3(0.0, 1.0, 0.0),
     vec3(0.0, 0.0, 1.0) );
-*/
+
+// triangle vertex
 const std::vector<Vertex> vertices = {
     {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
     {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
     {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+};
+*/
+// Rectangle (upper-right + bottom-left triangles) for showing use of index buffer https://vulkan-tutorial.com/en/Vertex_buffers/Index_buffer
+const std::vector<Vertex> vertices = {
+    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}}, // Top Left is red
+    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}}, // top-right is green
+    {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}, // bottom-right is blue
+    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}} // bottom-left is white
+};
+
+const std::vector<uint16_t> indices = {
+    0, 1, 2, // upper-right triangle
+    2, 3, 0 // bottom-left triangle.
 };
 
 class HelloTriangleApplication {
@@ -215,6 +229,7 @@ private:
         createFramebuffers();
         createCommandPool(); // https://vulkan-tutorial.com/en/Drawing_a_triangle/Drawing/Command_buffers
         createVertexBuffer(); // https://vulkan-tutorial.com/en/Vertex_buffers/Vertex_buffer_creation
+        createIndexBuffer();
         createCommandBuffers();
         createSyncObjects();
     }
@@ -851,9 +866,18 @@ private:
         VkBuffer vertexBuffers[] = {vertexBuffer};
         VkDeviceSize offsets[] = {0};
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
-            
+
+        // Using an index buffer https://vulkan-tutorial.com/en/Vertex_buffers/Index_buffer
+        vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
         // draw command for the triangle:
-        vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+        //vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+        
+        // change the drawing command to tell Vulkan to use the index buffer. Remove the vkCmdDraw line and replace it with vkCmdDrawIndexed:
+        vkCmdDrawIndexed(commandBuffer,
+                        static_cast<uint32_t>(indices.size()), // number of indices represents the number of vertices passed to the vertex shader.
+                        1, 0, 0, 0);
+
 
         vkCmdEndRenderPass(commandBuffer);
 
@@ -894,7 +918,7 @@ private:
             throw std::runtime_error("failed to allocate vertex buffer memory!");
         }
         
-        std::cout << "VERTEX_BUFFER allocationSize=" << memRequirements.size << std::endl;
+        std::cout << "CreateBuffer allocationSize=" << memRequirements.size << std::endl;
         
         // associate this bufferMemory with the buffer
         vkBindBufferMemory(device, buffer, bufferMemory, 0);
@@ -998,6 +1022,34 @@ private:
         but it doesn't mean that they are actually visible on the GPU yet. The transfer of data to the GPU is an operation that
         happens in the background and the specification simply tells us that it is guaranteed to be complete as of the next call to vkQueueSubmit.
         */
+    }
+    
+    // Index buffer creation https://vulkan-tutorial.com/en/Vertex_buffers/Index_buffer
+    void createIndexBuffer() {
+        
+        VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+        
+        // Using a staging buffer https://vulkan-tutorial.com/en/Vertex_buffers/Index_buffer
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer, stagingBufferMemory); // VK_BUFFER_USAGE_TRANSFER_SRC_BIT: Buffer can be used as source in a memory transfer operation.
+
+        // Memory-mapped I/O
+        void* data;
+        vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, indices.data(), (size_t) bufferSize);
+        vkUnmapMemory(device, stagingBufferMemory);
+
+        // Create device local indexBuffer
+        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+                    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+        // We copy data from the indices data from stagingBuffer to device local indexBuffer
+        copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+        
+        vkDestroyBuffer(device, stagingBuffer, nullptr);
+        vkFreeMemory(device, stagingBufferMemory, nullptr);
     }
     
     // Graphics cards can offer different types of memory to allocate from.
@@ -1247,6 +1299,9 @@ private:
         // cleanup swapChainFramebuffers, swapChainImageViews, v
         cleanupSwapChain();
         
+        vkDestroyBuffer(device, indexBuffer, nullptr);
+        vkFreeMemory(device, indexBufferMemory, nullptr);
+    
         vkDestroyBuffer(device, vertexBuffer, nullptr);
         vkFreeMemory(device, vertexBufferMemory, nullptr);
 
@@ -1504,6 +1559,8 @@ private:
     
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
     
     std::vector<VkSemaphore> imageAvailableSemaphores;
     std::vector<VkSemaphore> renderFinishedSemaphores;
